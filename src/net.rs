716be368b075350e133115d9ec8f396a0d0541d0
Diego Florio (I) <diegoefe@gmail.com>
2024-01-09 12:29:05 -0300

    Fixed Network/Proxy functions

--- src/net.rs
+++ src/net.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 use log::*;
 
-use crate::get_app_id;
+use crate::get_proxy_password_var;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Proxy {
@@ -26,8 +26,8 @@ impl Proxy {
             "USER"
         }
     }
-    fn get_proxy_user(pxy:&Proxy) -> Option<String> {
-        if let Some(user) = &pxy.user {
+    pub fn get_user(&self) -> Option<String> {
+        if let Some(user) = &self.user {
             Some(user.to_owned())
         } else if let Some(euser) = Proxy::get_env_var(Proxy::get_user_var()) {
             Some(euser)
@@ -35,20 +35,19 @@ impl Proxy {
             None
         }
     }
-    fn get_proxy_password(pxy:&Proxy) -> Option<String> {
-        let proxy_pass_var = format!("{}_PP", get_app_id().expect("To get app id!"));
-        if let Some(pass) = &pxy.password {
+    pub fn get_password(&self) -> Option<String> {
+        if let Some(pass) = &self.password {
             Some(pass.to_owned())
-        } else if let Some(epass) = Proxy::get_env_var(&proxy_pass_var) {
+        } else if let Some(epass) = Proxy::get_env_var(&get_proxy_password_var()) {
             Some(epass)
         } else {
             None
         }
     }
-    pub fn get_proxy_url(pxy:&Proxy) -> String {
-        let mut url = pxy.url.to_owned();
-        if let Some(user) = Proxy::get_proxy_user(pxy) {
-            if let Some(pass) = Proxy::get_proxy_password(pxy) {
+    pub fn get_url(&self) -> String {
+        let mut url = self.url.to_owned();
+        if let Some(user) = self.get_user() {
+            if let Some(pass) = self.get_password() {
                 let parts:Vec<&str> = url.split("://").collect();
                 let def_proto = format!("http://");
                 let mut proto = def_proto.as_str();
@@ -63,7 +62,7 @@ impl Proxy {
                                 user,
                                 pass,
                                 host,
-                                pxy.port);
+                                self.port);
             }
             debug!("proxy url [{url}]");
         }
@@ -74,4 +73,14 @@ impl Proxy {
 pub struct Network {
     pub proxy: Option<Proxy>,
     pub skip_ssl_validation: Option<bool>,
+}
+
+impl Network {
+    pub fn skip_ssl_validation(&self) -> bool {
+        if let Some(ssv) = self.skip_ssl_validation {
+            ssv
+        } else {
+            false
+        }
+    }
 }

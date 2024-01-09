db9b314062a29892b5c9818eba1f0d0d9d3292af
Diego Florio (I) <diegoefe@gmail.com>
2024-01-09 12:29:34 -0300

    Added CFGResolver to fetch config file name

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,20 @@
 
 /// Defines global state for this library
 mod data;
-pub use data::{get_app_id, set_app_id};
+pub use data::{
+    get_app_id,
+    set_app_id,
+    get_proxy_password_var,
+    get_filename
+};
 
 /// Defines common network configurations
 mod net;
 pub use net::{Network, Proxy};
+
+mod util;
+pub use util::{
+    CFGResolver,
+    read_yaml_from_file as read_config_from_yaml,
+    get_config_file_name,
+};
---
+++ src/util.rs
@@ -0,0 +1,54 @@
+use serde::Deserialize;
+use std::error::Error;
+use std::path::Path;
+use yaml_merge_keys::*;
+use std::fs::{self};
+use log::*;
+
+use crate::{get_app_id, get_filename};
+
+/// Deserialize YAML object from String
+pub fn read_yaml_from_string<T:for<'de> Deserialize<'de> >(str: &str) -> Result<T, Box<dyn Error>> {
+    use yaml_merge_keys::serde_yaml::Value;
+    let sy:Value = serde_yaml::from_str(str)?;
+    let v: Result<Value, serde_yaml::Error> = serde_yaml::to_value(sy);
+    
+    let fix: Result<Value, MergeKeyError> = merge_keys_serde(v?);
+    Ok(serde_yaml::from_value(fix?)?)
+}
+
+/// Read file into String and deserialize YAML object
+pub fn read_yaml_from_file<T:for<'de> Deserialize<'de>, P: AsRef<Path>>(path: P) -> Result<T, Box<dyn Error>> {
+    let sfile:String = fs::read_to_string(path)?.parse()?;
+    read_yaml_from_string(&sfile)
+}
+
+pub trait CFGResolver {
+    fn get_from_argument(&self) -> Option<&str>;
+}
+
+pub fn get_config_file_name(resolver:&impl CFGResolver) -> String {
+    let config = match resolver.get_from_argument() {
+        Some(cfg)=>cfg.to_string(),
+        None=>{
+            use std::env::*;
+            let app_id = get_app_id().expect("To get app id!");
+            match var(format!("{}_CONF_PATH", app_id).as_str()) {
+                Ok(cfg)=>cfg,
+                Err(_)=>{
+                    let dir = match var(format!("{}_CONF_DIR", app_id).as_str()) {
+                        Ok(d) => d,
+                        Err(_)=>".".to_string()
+                    };
+                    let file = match var(format!("{}_CONF_FILE", app_id).as_str()) {
+                        Ok(f)=>f,
+                        Err(_)=>get_filename()
+                    };
+                    format!("{}/{}", dir, file)
+                }
+            }
+        }
+    };
+    debug!("Using config file '{config}'");
+    config
+}
